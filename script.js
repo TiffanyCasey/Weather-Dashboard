62f8eeda627ec1c7ea5e79f0a7ef8cda5fcaa441
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-04-08 18:07:30 -0400

    working on JS

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
 $(document).ready(function() {
     console.log("ready!");
 
-// QueryURL to Opean Weather App 
-var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" +
-    "45e45c0bb2ef540df33fa21a29aafa8a";
-          
+// Global variables
+var cityInput=""; 
+var queryUrl="";
 
-//AJAX call 
-$.ajax({
-    url: queryUrl,
-    method: "GET",
-}).then(function(response) {
-    console.log(response);
-    for (var i = 0; i < response.data.length; i++) { // Loop through responses 
-        console.log("TITLE", response.data[i]. titile);
-    }
-});
-});
+storeCities();  
 
-//
+ // On-click when user enters city 
+ $("#cityInput").on("click", function(event) {
+    event.preventDefault();
+    console.log("you've clicked a button");
 
-//
+    // Grabs the input from the ingredint input text (get)
+    cityInput = $("#input").val();
+    console.log(cityInput, "cityInput")
+    }); // End of city button on-click 
+
+    // Function to retrieve city
+    function storeCities () {
+
+    // QueryURL to Open Weather App 
+    var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" 
+    + cityInput + "&appid=" + "45e45c0bb2ef540df33fa21a29aafa8a";  
+    console.log(queryUrl);
+
+    //AJAX call 
+    $.ajax({
+        url: queryUrl,
+        method: "GET",
+    }).then(function(response) {
+        console.log(response);
+        console.log(response.city)
+         }) // End of ajax then response
+        } // End of store Cities function 
+
+    // Append details to the site 
+    $("#dailyWeather").append(
+    //styling
+    "<div class='col s12 m6'>"
+
+    // Weather contents 
+    +  "<h2>" + response.city + "</h2>"
+
+    //close out card styling 
+    + "</div>"
+    ); // End of append 
+
+}); // end of document ready function
+
