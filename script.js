1a7d26af4e381bc5fd4082853be1c9894f9912b0
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-04-13 16:08:09 -0400

    local storage now working and daily web api working. Now owrking on 5-day forcast

--- script.js
+++ script.js
@@ -1,51 +1,68 @@
 $(document).ready(function() {
-    console.log("ready!");
+console.log("ready!");
 
 // Global variables
-var cityInput=""; 
-var queryUrl="";
+var currentDate = moment().format('M/DD/YYYY');  // Current Date
 
-storeCities();  
+renderCityList(); // Retrieves stored cities 
 
- // On-click when user enters city 
- $("#cityInput").on("click", function(event) {
-    event.preventDefault();
-    console.log("you've clicked a button");
+// Function to get Cities from local storage 
+function renderCityList() {
+  localStorage.getItem("cityInput"); 
+  }
 
-    // Grabs the input from the ingredint input text (get)
-    cityInput = $("#input").val();
-    console.log(cityInput, "cityInput")
-    }); // End of city button on-click 
+// On-click when user enters city 
+$("#basic-text1").on("click", function(event) {
+  event.preventDefault();
+  console.log("you've clicked a button");
+  var cityInput = $("#input").val(); //saves the city that has been entered
+  localStorage.setItem("cityInput", cityInput); //saves city input to local storage 
 
-    // Function to retrieve city
-    function storeCities () {
+  // Append List of Cities to Web Page 
+  $("#cityButtons").append (
+    //styling 
+    "<div class='list-group'>"
 
-    // QueryURL to Open Weather App 
-    var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" 
-    + cityInput + "&appid=" + "45e45c0bb2ef540df33fa21a29aafa8a";  
-    console.log(queryUrl);
+  // City text
+  + "<a href='#' class='list-group-item'>" + cityInput + "</a>"
+  + "</div>")
 
-    //AJAX call 
-    $.ajax({
-        url: queryUrl,
-        method: "GET",
-    }).then(function(response) {
-        console.log(response);
-        console.log(response.city)
-         }) // End of ajax then response
-        } // End of store Cities function 
+  renderCityList(); // Retrieves stored cities 
+}); // End of city button on-click
 
+  // QueryURL to Open Weather App for One Day 
+  var oneDay ="https://api.openweathermap.org/data/2.5/weather?q=" 
+  + "Newark" + "&units=imperial" + "&appid=45e45c0bb2ef540df33fa21a29aafa8a";
+  console.log("oneDay", oneDay);  
+
+  //AJAX call for One Day
+  $.ajax({
+      url: oneDay,
+      method: "GET",
+  }).then(function(response) {
+      console.log("response", response);
+
+    //icon url  
+    var iconUrl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+   
     // Append details to the site 
     $("#dailyWeather").append(
-    //styling
-    "<div class='col s12 m6'>"
+      //styling
+      "<div class='col s12 m6'>"
+  
+      // Weather contents 
+      +  "<h2>" + response.name + " (" + currentDate + ")" + "&nbsp" + "<img src='" + iconUrl  + "'>" + "</h2>"
+      +  "<ul>" + "Temperature: " +  response.main.temp + " °F" + "</ul>"
+      +  "<ul>" + "Humidity: " + response.main.humidity + "%" + "</ul>"
+      +  "<ul>" + "Wind Speed: " +  response.wind.speed + " MPH" + "</ul>"
+      +  "<ul>" + "UV Index: " + "</ul>"
+  
+      //close out card styling 
+      + "</div>"
+      ); // End of append 
+     }) // End of ajax then response  
 
-    // Weather contents 
-    +  "<h2>" + response.city + "</h2>"
 
-    //close out card styling 
-    + "</div>"
-    ); // End of append 
-
+  
 }); // end of document ready function
 
